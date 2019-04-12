157221c0514ba113442df7129e3c2f04d0f9bfbb
Chonim <chihoyoon1217@gmail.com>
2019-04-12 18:18:02 +0900

    create beer data

---
+++ src/assets/data/beers.js
@@ -0,0 +1,82 @@
+export default [{
+  id: 0,
+  name: 'Bellevue Kriek',
+  image: 'bellevue-kriek.png',
+  tags: [{
+    key: 2,
+    name: '람빅'
+  }, {
+    key: 4,
+    name: '수입맥주'
+  }],
+  price: 7000,
+  stock: 5
+}, {
+  id: 1,
+  name: 'Cass Light',
+  image: 'cass-light.png',
+  tags: [{
+    key: 0,
+    name: '라거'
+  }, {
+    key: 3,
+    name: '국산맥주'
+  }],
+  price: 4000,
+  stock: 10
+}, {
+  id: 2,
+  name: 'Cass',
+  image: 'cass.png',
+  tags: [{
+    key: 0,
+    name: '라거'
+  }, {
+    key: 3,
+    name: '국산맥주'
+  }],
+  price: 3500,
+  stock: 14
+}, {
+  id: 3,
+  name: 'Framboise',
+  image: 'framboise-kriek-lambic.jpg',
+  tags: [{
+    key: 2,
+    name: '람빅'
+  }, {
+    key: 4,
+    name: '수입맥주'
+  }],
+  price: 7500,
+  stock: 4
+}, {
+  id: 4,
+  name: 'OB Premier',
+  image: 'ob-premier.png',
+  tags: [{
+    key: 0,
+    name: '라거'
+  }, {
+    key: 3,
+    name: '국산맥주'
+  }, {
+    key: 5,
+    name: 'OB맥주'
+  }],
+  price: 4000,
+  stock: 12
+}, {
+  id: 5,
+  name: 'Sierra Nevada',
+  image: 'sierra-nevada.jpg',
+  tags: [{
+    key: 1,
+    name: '에일'
+  }, {
+    key: 4,
+    name: '수입맥주'
+  }],
+  price: 10000,
+  stock: 3
+}]
