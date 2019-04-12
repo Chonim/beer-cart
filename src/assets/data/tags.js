694019b3d5f2e5b59215160d1bd7fa8a68655eb4
Chonim <chihoyoon1217@gmail.com>
2019-04-12 16:02:42 +0900

    create dummy tags

---
+++ src/assets/data/tags.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    key: 0,
+    name: '라거'
+  },
+  {
+    key: 1,
+    name: '에일'
+  },
+  {
+    key: 2,
+    name: '람박'
+  },
+  {
+    key: 3,
+    name: '국산맥주'
+  },
+  {
+    key: 4,
+    name: '수입맥주'
+  },
+  {
+    key: 5,
+    name: 'OB맥주'
+  }
+]
