9824a3c1ccd39733cb1f023c814a68ec3f86b5db
Chonim <chihoyoon1217@gmail.com>
2019-04-13 15:55:40 +0900

    add tags filter getter

--- src/store/modules/beer/getters.js
+++ src/store/modules/beer/getters.js
@@ -1,5 +1,16 @@
+import store from '@/store'
+import {
+  sortByFilters
+} from '@/utils/tags-filter'
+
 export default {
-  getBeerList: state => state.beerList,
+  getBeerList: state => {
+    if (store.getters['tag/isAllTagsChecked']) {
+      return state.beerList
+    }
+    const checkedTags = store.getters['tag/getCheckedTags']
+    return sortByFilters(checkedTags, state.beerList)
+  },
   getCartItems: state => state.cartItems,
   getCartLength: state => state.cartItems.length
 }
--- src/store/modules/tag/getters.js
+++ src/store/modules/tag/getters.js
@@ -1,3 +1,9 @@
+import {
+  getCheckedTags
+} from '@/utils/tags-filter'
+
 export default {
-  getTagList: state => state.tagList
+  getTagList: state => state.tagList,
+  getCheckedTags: state => getCheckedTags(state.tagList),
+  isAllTagsChecked: state => state.tagList.every(tag => tag.isChecked)
 }
