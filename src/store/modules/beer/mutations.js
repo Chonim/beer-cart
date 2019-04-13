eae48846668cfdfdabbe8116199973a4f283ba43
Chonim <chihoyoon1217@gmail.com>
2019-04-13 17:21:50 +0900

    fix subtract from cart bug

--- src/store/modules/beer/mutations.js
+++ src/store/modules/beer/mutations.js
@@ -4,21 +4,27 @@ export default {
   },
   updateCart: (state, payload) => {
     const { id, amount } = payload
+    // add: stock--, inCart++
+    // subtract: stock++, inCart--
     payload.stock -= amount
     payload.inCart += amount
-    const matchedItem = state.cartItems.find(item => item.id === id)
-    if (!matchedItem) {
+    const matchedItemIndex = state.cartItems.findIndex(item => item.id === id)
+    // push to cartItems if it's not in the array
+    if (matchedItemIndex < 0) {
       state.cartItems.push(payload)
+      return
     }
-    if (matchedItem && !matchedItem.inCart) {
-      state.cartItems.splice(matchedItem, 1)
+    // splice from the array if it's subtracted and its inCart is 0
+    const matchedItem = state.cartItems[matchedItemIndex]
+    if (amount < 0 && matchedItem.inCart === 0) {
+      state.cartItems.splice(matchedItemIndex, 1)
     }
   },
   emptyCart: (state) => {
     state.cartItems = []
     state.beerList = state.beerList.map(item => ({
       ...item,
-      stock: item.stock + item.inCart,
+      // stock: item.stock + item.inCart, // reset stock to the initial amount
       inCart: 0
     }))
   }
